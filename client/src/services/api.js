5a2b5f5948bb988efd92bdb2bdf53029c8a5a27a
PanasaChaitanyavarma <panasachaitanya@gmail.com>
2026-07-22 14:47:11 +0530

    changed backend port

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://flexihire-knw5.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
